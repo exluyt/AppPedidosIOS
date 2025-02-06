add188b9e9eed8470598f283b12a88f7306d4477
Usuario invitado <Guest@B213-06.local>
2025-02-06 16:20:54 +0100

    Estructura iOS

---
+++ AppPedidos/AppPedidosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPedidosApp.swift
+//  AppPedidos
+//
+//  Created by Usuario invitado on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct AppPedidosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPedidosTests/AppPedidosTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppPedidosTests.swift
+//  AppPedidosTests
+//
+//  Created by Usuario invitado on 6/2/25.
+//
+
+import Testing
+@testable import AppPedidos
+
+struct AppPedidosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
