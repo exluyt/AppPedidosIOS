ac83ac53a2df39517b8b7825fe84c74030a6432c
Usuario invitado <Guest@B213-05.local>
2025-02-13 17:20:12 +0100

    muchas cosas

--- AppPedidos/ContentView.swift
+++ AppPedidos/ContentView.swift
@@ -10,13 +10,70 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack{
+                Text("PlaceHolder")
+                    
+                Spacer()
+                Button{
+                    
+                }label: {
+                    Image(systemName: "magnifyingglass")
+                }
+                
+                Button{
+                    
+                }label: {
+                    Image(systemName: "cart.fill")
+                }
+                Button{
+                    
+                }label: {
+                    Image(systemName: "person.circle.fill")
+                }
+                
+            }
+            .font(.custom("Geist-Black", size: 24))
+            .accentColor(Color.black)
+            .padding(8)
+            .overlay(
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.black),
+                alignment: .bottom
+            )
+            Rectangle()
+                .frame(width:UIScreen.main.bounds.width)
+                .aspectRatio(16/9, contentMode: .fit)
+            
+            Section{
+                Text("Waos")
+                    .frame(width: UIScreen.main.bounds.width * 0.9,
+                           alignment: .leading )
+                    .overlay(
+                        Rectangle()
+                            .frame(height: 1)
+                            .foregroundStyle(.black),
+                            alignment: .bottom
+                    )
+                    
+                Spacer()
+                    .frame(height: 100)
+                
+            }
+            
+            Text("Waos 2")
+                .frame(width: UIScreen.main.bounds.width * 0.9,
+                       alignment: .leading )
+                .overlay(
+                    Rectangle()
+                        .frame(height: 1)
+                        .foregroundStyle(.black),
+                        alignment: .bottom
+                )
         }
-        .padding()
+        .padding(0)
     }
+
 }
 
 #Preview {
