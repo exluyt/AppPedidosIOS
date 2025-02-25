e35dc06367ded4f5db31e4a476f0aee49594d602
Usuario invitado <Guest@B213-06.local>
2025-02-25 21:06:19 +0100

    add: api services

--- AppPedidos/ContentView.swift
+++ AppPedidos/ContentView.swift
@@ -7,29 +7,117 @@
 
 import SwiftUI
 
+
+// Estructura del juego con conformidad a Codable
+struct Game: Identifiable, Codable {
+    let id = UUID()  // ID único para cada juego
+    let name: String
+    let rating: Double
+    let image: String
+}
+
+// Vista de modelo que maneja la lógica de los juegos
+class GameViewModel: ObservableObject {
+    @Published var games = [Game]()
+    @Published var strategyGames = [Game]() // Para juegos de estrategia
+    
+    func fetchGames(appName: String = "", category: String = "") {
+        guard let url = URL(string: "http://10.100.8.250:5000/buscar_app") else { return }
+        
+        // Si appName está vacío, no lo incluyes; si category tiene valor, lo incluyes
+        var parameters: [String: Any] = ["app_name": appName, "n_hits": 10]
+        
+        if !category.isEmpty {
+            parameters["category"] = category
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: [])
+        } catch {
+            print("Error al crear el JSON")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("Error en la solicitud: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                // Asumiendo que la respuesta es un array de juegos
+                if let responseArray = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] {
+                    var gamesArray: [Game] = []
+                    for gameData in responseArray {
+                        // Crear un objeto de tipo Game desde los datos de la respuesta
+                        if let name = gameData["Nombre"] as? String,
+                           let rating = gameData["Puntuación"] as? Double,
+                           let image = gameData["Imagenes"] as? [String],
+                           let firstImage = image.first {
+                            let game = Game(name: name, rating: rating, image: firstImage)
+                            gamesArray.append(game)
+                        }
+                    }
+                    DispatchQueue.main.async {
+                        if category.isEmpty {
+                            self.games = gamesArray // Juegos sin categoría
+                        } else {
+                            self.strategyGames = gamesArray // Juegos de categoría "strategy"
+                        }
+                    }
+                }
+            }
+        }.resume()
+    }
+}
+
+// Vista principal donde se muestran los juegos
+struct MainView: View {
+    @State private var isLoggedIn = false
+    
+    var body: some View {
+        NavigationStack {
+            if !isLoggedIn {
+                ContentView()
+            } else {
+                LoginView(isLoggedIn: $isLoggedIn)
+            }
+        }
+    }
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = GameViewModel()
+    
     var body: some View {
         VStack {
-            HStack{
+            HStack {
                 Text("PlaceHolder")
-                    
+                
                 Spacer()
-                Button{
-                    
-                }label: {
+                Button {
+                    // Acción de búsqueda
+                } label: {
                     Image(systemName: "magnifyingglass")
                 }
                 
-                Button{
-                    
-                }label: {
+                Button {
+                    // Acción de carrito
+                } label: {
                     Image(systemName: "cart.fill")
                 }
-                Button{
-                    
-                }label: {
+                
+                Button {
+                    // Acción de perfil
+                } label: {
                     Image(systemName: "person.circle.fill")
-                }	
+                }
                 
             }
             .font(.custom("Geist-Black", size: 24))
@@ -41,44 +129,111 @@ struct ContentView: View {
                     .foregroundColor(.black),
                 alignment: .bottom
             )
-            Rectangle()
-                .frame(width:UIScreen.main.bounds.width)
-                .aspectRatio(16/9, contentMode: .fit)
             
-            Section{
-                Text("Waos")
-                    .frame(width: UIScreen.main.bounds.width * 0.9,
-                           alignment: .leading )
+            // "Suggestions for you"
+            Section {
+                Text("Suggestions for you")
+                    .frame(width: UIScreen.main.bounds.width * 0.9, alignment: .leading)
                     .overlay(
                         Rectangle()
                             .frame(height: 1)
                             .foregroundStyle(.black),
-                            alignment: .bottom
+                        alignment: .bottom
                     )
                     .font(.custom("Geist-Medium", size: 20))
-                    
-                Spacer()
-                    .frame(height: 100)
                 
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(viewModel.games) { game in
+                            GameRow(game: game)
+                                .padding()
+                        }
+                    }
+                }
+                .frame(width: UIScreen.main.bounds.width * 0.9)
             }
             
-            Text("Waos 2")
-                .frame(width: UIScreen.main.bounds.width * 0.9,
-                       alignment: .leading )
+            // "Strategy"
+            Text("Strategy")
+                .frame(width: UIScreen.main.bounds.width * 0.9, alignment: .leading)
                 .overlay(
                     Rectangle()
                         .frame(height: 1)
                         .foregroundStyle(.black),
-                        alignment: .bottom
+                    alignment: .bottom
                 )
                 .font(.custom("Geist-Medium", size: 20))
-
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(viewModel.strategyGames) { game in
+                        GameRow(game: game)
+                            .padding()
+                    }
+                }
+            }
+            .frame(width: UIScreen.main.bounds.width * 0.9)
+        }
+        .onAppear {
+            viewModel.fetchGames(appName: "")  // Cargar juegos sin categoría para "Suggestions for you"
+            viewModel.fetchGames(category: "strategy")  // Cargar juegos de la categoría "strategy"
         }
         .padding(0)
         Spacer()
     }
-
 }
+
+// Vista para mostrar cada fila de un juego
+struct GameRow: View {
+    let game: Game
+
+    var body: some View {
+        VStack {
+            // Cargar la imagen de manera asíncrona desde la URL
+            AsyncImage(url: URL(string: game.image)) { phase in
+                switch phase {
+                case .empty:
+                    // Placeholder mientras se carga la imagen
+                    Image(systemName: "gamecontroller")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                case .success(let image):
+                    // Imagen cargada con éxito
+                    image.resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                case .failure:
+                    // Error al cargar la imagen
+                    Image(systemName: "exclamationmark.triangle.fill")
+                        .foregroundColor(.red)
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                @unknown default:
+                    EmptyView()
+                }
+            }
+            
+            // Nombre del juego
+            Text(game.name)
+                .font(.footnote)
+                .frame(width: 100, height: 43, alignment: .topLeading)
+            
+            // Puntuación del juego
+            HStack {
+                Text(String(format: "%.1f", game.rating))
+                    .font(.footnote)
+                    .frame(alignment: .center)
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+                    .font(.footnote)
+                    .frame(alignment: .center)
+            }
+            .frame(width:100, height: 20, alignment: .topLeading)
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
---
+++ AppPedidos/Preview Content/LoginView.swift
@@ -0,0 +1,88 @@
+//
+//  LoginView.swift
+//  AppPedidos
+//
+//  Created by Usuario invitado on 25/2/25.
+//
+
+import SwiftUI
+import FirebaseAuth
+import CryptoKit
+
+struct LoginView: View {
+    @Binding var isLoggedIn: Bool
+    @State private var email = ""
+    @State private var password = ""
+    @State private var errorMessage = ""
+    private let login: [String: String] = ["ex@gmail.com": "03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4"]
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .font(.custom("AppLogo", size: 80))
+            Form {
+                Section {
+                    HStack {
+                        Text("Email")
+                        Spacer(minLength: 55)
+                        TextField("example@gmail.com", text: $email)
+                            .keyboardType(.emailAddress)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                            .autocapitalization(.none)
+                    }
+                    HStack {
+                        Text("Contraseña")
+                        Spacer()
+                        SecureField("********", text: $password)
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+                    }
+                }
+                .listRowBackground(Color.gray)
+            }
+            .frame(maxHeight: 150)
+            .scrollDisabled(true)
+            .scrollContentBackground(.hidden)
+            
+            if !errorMessage.isEmpty {
+                Text(errorMessage)
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            Button("Login") {
+                loginUser(email: email, password: password)
+            }
+            .buttonStyle(.borderedProminent)
+            .padding()
+        }
+        .padding()
+    }
+    
+    func loginUser(email: String, password: String) {
+        let hashedPwd = hashPassword(password: password)
+        if checkLoginArray(email: email, password: hashedPwd) {
+            isLoggedIn = true
+        } else {
+            Auth.auth().signIn(withEmail: email, password: password) { _, error in
+                if error == nil {
+                    isLoggedIn = true
+                } else {
+                    errorMessage = "Error: Usuario o contraseña incorrectos"
+                }
+            }
+        }
+    }
+    
+    func checkLoginArray(email: String, password: String) -> Bool {
+        return login[email] == password
+    }
+    
+    func hashPassword(password: String) -> String {
+        let passwordData = Data(password.utf8)
+        let hashed = SHA256.hash(data: passwordData)
+        return hashed.map { String(format: "%02hhx", $0) }.joined()
+    }
+    
+    
+}
